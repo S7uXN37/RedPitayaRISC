4bf8012a10b62df521e81a32b317a2e64e7cab8f
S7uXN37 <S7uXN37@users.noreply.github.com>
2021-07-01 19:23:54 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
+# Compile controller
+echo "Compiling controller."
+cd risc_v_app
+make INSTALL_DIR=/opt/redpitaya
+
 # Distribute code into correct folders
 if [ -e "/opt/redpitaya/www/apps/risc_v_app" ]; then
     echo "Found earlier app version. Removing."
     rm -r "/opt/redpitaya/www/apps/risc_v_app"
+    # Possibly also remove files from fpga and lib
 fi
 echo "Copying files."
 cp -r risc_v_app /opt/redpitaya/www/apps/risc_v_app
 cp fpga/* /opt/redpitaya/www/apps/fpga
 cp lib/* /opt/redpitaya/www/apps/lib
-
-# Compile controller
-echo "Compiling controller."
-cd risc_v_app
-make INSTALL_DIR=/opt/redpitaya
 
 echo "Installation complete."
